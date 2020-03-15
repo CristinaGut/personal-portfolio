5e56a944bcee4119624769410947c268359469ab
Cristina Gutierrez <cristy.g425@gmail.com>
2020-03-14 23:20:05 -0600

    Added data files

---
+++ star-wars/starwars.js
@@ -0,0 +1,9 @@
+import { films } from '.../data/films.js'
+
+console.log('Hi! My first JavaScript code.') 
+
+console.log(document.querySelector('.greeting'))
+
+let greetingDiv = document.querySelector('.greeting')
+
+greetingDiv.textContent = 'Welcome to my web page.'
