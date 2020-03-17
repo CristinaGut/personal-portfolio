cbd2d993d278d846e5dfce4e1f17a584e55cc93a
Cristina Gutierrez <cristy.g425@gmail.com>
2020-03-16 22:38:59 -0600

    Added character images

--- star-wars/starwars.js
+++ star-wars/starwars.js
@@ -1,4 +1,5 @@
-import { films } from '.../data/films.js'
+import { films } from '../data/films.js'
+import { people } from '../data/people.js'
 
 console.log('Hi! My first JavaScript code.') 
 
@@ -6,4 +7,20 @@ console.log(document.querySelector('.greeting'))
 
 let greetingDiv = document.querySelector('.greeting')
 
-greetingDiv.textContent = 'Welcome to my web page.'
+greetingDiv.textContent ="StarWars Cast"
+
+let castList = document.createElement("li")
+
+let counter = 1
+
+people.forEach(person => {
+    let nameItem = document.createElement("li")
+    nameItem.textContent = person.name
+    castList.appendChild(nameItem)
+    let personImg = document.createElement("img")
+    personImg.src = `http://starwars-visualguide.com/assets/img/characters/${counter}.jpg`
+    greetingDiv.appendChild(personImg)
+    counter++
+})
+
+greetingDiv.appendChild(castList)
