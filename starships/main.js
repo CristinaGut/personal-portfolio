415ed941e6d9b946f391eece5be69f451c374e1e
Cristina Gutierrez <cristy.g425@gmail.com>
2020-03-17 23:41:17 -0600

    Fix starships list

--- starships/main.js
+++ starships/main.js
@@ -1,26 +1,15 @@
 import { starships } from '../data/starships.js'
-import { removeChildren, getLastNumber } from '../util.js'
 
 const nav = document.querySelector('.nav')
 
 const navList = document.querySelector('.navList')
 
-const shipView = document.querySelector('#main')
-
 function populateNav(starships) {
     starships.forEach(starship => {
         let shipAnchor = document.createElement('a')
         shipAnchor.href = '#'
-        let listenItem = document.createElement('li')
-        listenItem.textContent = starship.getLastNumber
-
-        shipAnchor.addEventListener('click', event => {
-            //store name of the list item clicked on
-        let shipName = event.target.textContent
-        const foundShip = starships.find(ship => ship.name === shipName )
-        console.log(foundShip)
-        populateShipView(foundship)
-        })
+        let listItem = document.createElement('li')
+        listItem.textContent = starship.name
 
         shipAnchor.appendChild(listItem)
         navList.appendChild(shipAnchor)
@@ -29,12 +18,5 @@ function populateNav(starships) {
     nav.appendChild(navList)
 }
 
-function populateShipView(shipData) {
-    removeChildren(shipView)
-    let imageNum = getLastNumber(shipData.url)
-    let shipImage = document.createElement('img')
-    shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${imageNum}.jpg`
-    shipView.appendChild(shipImage)
-}
 
-populateNav(starships) 
+populateNav(starships)
