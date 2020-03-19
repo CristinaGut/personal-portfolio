ecb1dc5e3d35db2d8cb09a3fab6de071933fc69d
Cristina Gutierrez <cristy.g425@gmail.com>
2020-03-19 17:15:17 -0600

    Modify starships

--- starships/main.js
+++ starships/main.js
@@ -1,11 +1,11 @@
 import { starships } from '../data/starships.js'
-import { removeChildren } from '../util.js'
+
 
 const nav = document.querySelector('.nav')
 
 const navList = document.querySelector('.navList')
 
-const shipView = document.querySelector('#main')
+
 
 function populateNav(starships) {
     starships.forEach(starship => {
@@ -14,29 +14,11 @@ function populateNav(starships) {
         let listItem = document.createElement('li')
         listItem.textContent = starship.name
 
-        shipAnchor.addEventListener('click', event => {
-            let shipName = event.target.textContent
-            populateShipView(foundShip)
-        })
-
         shipAnchor.appendChild(listItem)
         navList.appendChild(shipAnchor)
 
     })
     nav.appendChild(navList)
-}
-
-function populateShipView(shipData) {
-    removeChildren(shipView)
-    let shipTitle = document.createElement('h1')
-    shipTitle.textContent = Data
-    shipView.appendChild(shipTitle)
-
-
-   /*  let imageNum = getLastNumber(shipData.url)
-    let shipImage = document.createElement('img')
-    shipImage.src = `http://starwars-visualguide.com/assets/img/characters/${imageNum}.jpg`
-    shipView.appendChild(shipImage) */
 }
 
 populateNav(starships)
