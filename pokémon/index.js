ccb0607f57dc2ac5cd5409b89c3ff5613c331fc6
Cristina Gutierrez <cristy.g425@gmail.com>
2020-04-28 20:46:56 -0600

    Deleted extra card

--- pokémon/index.js
+++ pokémon/index.js
@@ -21,7 +21,7 @@ async function getAPIData(url) {
 }
 
 function loadPage() {
-    getAPIData('https://pokeapi.co/api/v2/pokemon/?&limit=151').then(
+    getAPIData('https://pokeapi.co/api/v2/pokemon/?&limit=150').then(
         (data) => {
             for (const pokemon of data.results) {
                 getAPIData(pokemon.url).then(
